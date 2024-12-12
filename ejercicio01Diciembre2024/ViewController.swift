ec04409b81936c367dbcf7eb082a914d01c697fc
Eduardo Segovia <edu92seg@gmail.com>
2024-12-12 02:41:45 -0800

    Ejercicio acabado

--- ejercicio01Diciembre2024/ViewController.swift
+++ ejercicio01Diciembre2024/ViewController.swift
@@ -6,14 +6,37 @@
 //
 
 import UIKit
+import Toast
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var txtNombre: UITextField!
+    @IBOutlet weak var txtPreguntasTotales: UITextField!
+    @IBOutlet weak var txtPreguntasAcertadas: UITextField!
+    @IBOutlet weak var txtIndice: UITextField!
+    @IBOutlet weak var lblNotaFinal: UILabel!
+    
+    var listaExamenes:[Examen] = [];
+        var indice = 0;
+        var nota = 0.0;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func btnInsertarExamen(_ sender: Any) {
+        nota = Double((Float(txtPreguntasAcertadas.text!)!)/(Float(txtPreguntasTotales.text!)!)*10)
+                
+                let nuevoExamen: Examen = Examen(nombre: txtNombre.text!, preguntasTotales: Int(txtPreguntasTotales.text!)!, acertadas: Int(txtPreguntasAcertadas.text!)!, notaFinal: Float(nota));
+                listaExamenes.append(nuevoExamen);
+        self.view.makeToast("Examen de \(nuevoExamen.nombre) agregado.")
+        
+    }
+    @IBAction func btnVerExamen(_ sender: Any) {
+        indice = Int(txtIndice.text!)!;
+                
+                lblNotaFinal.text = "Alumno: \(listaExamenes[indice].nombre), \n\n Preguntas totales: \(listaExamenes[indice].preguntasTotales), \n\n Acertadas: \(listaExamenes[indice].acertadas). \n \n NOTA FINAL: \(listaExamenes[indice].notaFinal)"
+    }
+    
 }
 
---
+++ ejercicio01Diciembre2024/models/Examen.swift
@@ -0,0 +1,16 @@
+//
+//  Examen.swift
+//  ejercicio01Diciembre2024
+//
+//  Created by Eduardo Segovia Roman on 12/12/24.
+//
+
+import Foundation
+
+struct Examen  {
+    var nombre: String;
+    var preguntasTotales: Int;
+    var acertadas: Int;
+    var notaFinal: Float;
+    
+}
