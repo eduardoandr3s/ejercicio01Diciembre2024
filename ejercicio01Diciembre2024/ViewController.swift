83705f07f3284c113c495d8a3b767babd1b32405
Eduardo Segovia <edu92seg@gmail.com>
2024-12-12 01:10:20 -0800

    Initial Commit

---
+++ ejercicio01Diciembre2024/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ejercicio01Diciembre2024
+//
+//  Created by Eduardo Segovia Roman on 12/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
